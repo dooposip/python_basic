455276cc8f572cfc71e64736dde20b416701e22f
unknown <posipdoo>
2025-08-29 12:34:09 +0900

    20250829

---
+++ module/ex2.py
@@ -0,0 +1,8 @@
+import mod2
+
+result = mod2.add(3,4)
+print(result)
+
+
+result2 = mod2.math()
+print(result2.solv(10))
---
+++ module/mymod/mod2.py
@@ -0,0 +1,11 @@
+# mod2.py
+PI = 3.141592 #상수
+
+# 클래스
+class math:
+    def solv(self, r):
+        return PI * (r**2)
+
+# 함수
+def add(a,b):
+    return a+b
